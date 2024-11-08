f2295fbda8c8c1097fa8d746d101b572a9c36c2c
Propo41 <aliahnaf327@gmail.com>
2024-11-08 13:22:40 +0600

    fixed issue with env variables and added npm-run-all2 for convenience

--- client/src/config/secrets.ts
+++ client/src/config/secrets.ts
@@ -1,14 +1,13 @@
+// https://vite.dev/guide/env-and-mode.html
 export const secrets = {
-  nodeEnvironment: import.meta.env.NODE_ENV,
-  appEnvironment: import.meta.env.REACT_APP_ENVIRONMENT,
-  mockCalender: import.meta.env.REACT_APP_MOCK_CALENDER,
-  clientId: import.meta.env.REACT_APP_CLIENT_ID,
-  oAuthRedirectUrl: import.meta.env.REACT_APP_REDIRECT_URI,
-  backendEndpoint: import.meta.env.REACT_APP_BACKEND_ENDPOINT,
-  appSlogan: import.meta.env.REACT_APP_APP_SLOGAN,
-  appTitle: import.meta.env.REACT_APP_APP_TITLE,
+  nodeEnvironment: import.meta.env.MODE,
+  appEnvironment: import.meta.env.VITE_ENVIRONMENT,
+  mockCalender: import.meta.env.VITE_MOCK_CALENDER,
+  clientId: import.meta.env.VITE_CLIENT_ID,
+  oAuthRedirectUrl: import.meta.env.VITE_REDIRECT_URI,
+  backendEndpoint: import.meta.env.VITE_BACKEND_ENDPOINT,
+  appSlogan: import.meta.env.VITE_APP_SLOGAN,
+  appTitle: import.meta.env.VITE_APP_TITLE,
 };
 
-// todo: https://vite.dev/guide/env-and-mode.html
-// process not defined
 console.log(secrets);
